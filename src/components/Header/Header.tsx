d0e5e1f7c01f911048a24e34ce30c1eb8c6e8bc5
Maximiliano Giménez <maxxi02gimenez@gmail.com>
2025-09-10 21:48:23 -0300

    header dividers added for lower resolutions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,14 +12,23 @@ export default function Header() {
           <img className="brand-logo" src={logo} alt="brand-logo" />
           <h2>DESCUBRI-2</h2>
         </div>
+
+        <hr className="divisor" />
+
         <div className="nav-link-container">
           <div className="nav-link">
             <p className="nav-link-text">HOME</p>
           </div>
           <div className="nav-link">
+            <p className="nav-link-text">DESCUBRIR</p>
+          </div>
+          <div className="nav-link last-link">
             <p className="nav-link-text">CONTACTO</p>
           </div>
         </div>
+
+        <hr className="divisor" />
+
         <div className="favourites" onClick={() => navigate("/favourites", {})}>
           <h2>Favoritos ⭐</h2>
         </div>
